36586c6a7d0bfca1c944d1b86cf51ab0f281f690
pvosper <pvosper@mac.com>
2017-01-29 08:30:12 -0800

    WIP: Mostly works fine but needs better error handling

--- calculator.py
+++ calculator.py
@@ -31,15 +31,20 @@ html_text = """<html>
 <title>WSGI Calulator (Session 03 Homework)</title>
 </head>
 <body>
-<p>Path Info: {print_path_info} Entries: {print_no_entries}</p>
-<p>Operation: {print_operation} First Operand: {print_operand_1}
-Second Operand: {print_operand_2}</p>
-<h1>Result: {print_result}</h3>
+<h1>{print_operand_a} {print_operation_sign} {print_operand_b}
+= {print_result}</h1>
 <hr>
 <h3>Examples</h3>
 <p><a href="http://localhost:8080/multiply/3/5">multiply/3/5</a></p>
 <p><a href="http://localhost:8080/add/23/42">add/23/42</a></p>
 <p><a href="http://localhost:8080/divide/6/0">divide/6/0</a></p>
+<p><a href="http://localhost:8080/subtract/126/21">subtract/126/21</a></p>
+<p><a href="http://localhost:8080/divide/4223/17">divide/4223/17</a></p>
+<p><a href="http://localhost:8080/subtract/123/0">subtract/123/0</a></p>
+<hr>
+<p>Path Info: {print_path_info} Entries: {print_no_entries}</p>
+<p>Operation: {print_operation} First Operand: {print_operand_a}
+Second Operand: {print_operand_b}</p>
 </body>
 </html>"""
 
@@ -50,23 +55,62 @@ def application(environ, start_response):
         path_info = environ.get('PATH_INFO', None)
         if path_info is None:
             raise NameError
+
         args = resolve_path(path_info)
-        if args[0].strip() == "multiply":
-            result = int(args[1]) * int(args[2])
-        elif args[0].strip() == "divide":
-            result = int(args[1]) / int(args[2])
-        elif args[0].strip() == "add":
-            result = int(args[1]) + int(args[2])
-        elif args[0].strip() == "subtract":
-            result = int(args[1]) - int(args[2])
+
+        if len(args) != 3:
+            operation = "failed"
+            operation_sign = "f"
+        else:
+            pass
+
+        operation_list = ["multiply", "divide", "add", "subtract"]
+
+        if args[0].strip() in operation_list:
+            operation = args[0].strip()
+        else:
+            operation = "failed"
+            operation_sign = "f"
+
+        try:
+            operand_a = int(args[1])
+        except:
+            operand_a = "error"
+
+        try:
+            operand_b = int(args[2])
+        except:
+            operand_b = "error"
+
+        if operation == "multiply":
+            result = operand_a * operand_b
+            operation_sign = "*"
+        elif operation == "divide":
+            if operand_b == 0:
+                result = "bad request - can't divide by zero"
+                operation_sign = "/"
+            else:
+                result = operand_a / operand_b
+                operation_sign = "/"
+        elif operation == "add":
+            result = operand_a + operand_b
+            operation_sign = "+"
+        elif operation == "subtract":
+            result = operand_a - operand_b
+            operation_sign = "-"
+        elif operation == "failed":
+            result = "error - please try again"
+            operation_sign = "f"
         else:
             result = "failer"
+
         body = html_text.format(
             print_path_info=path_info,
             print_no_entries=len(args),
-            print_operation=args[0],
-            print_operand_1=args[1],
-            print_operand_2=args[2],
+            print_operation=operation,
+            print_operation_sign=operation_sign,
+            print_operand_a=operand_a,
+            print_operand_b=operand_b,
             print_result=result
         )
         status = "200 OK"
